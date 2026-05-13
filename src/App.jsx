14c2ef44ae92bd82d647f89b9988c900dcfff5af
Ujwal Shrestha <sthaujwal07@gmail.com>
2026-05-13 11:12:49 +0545

    useState creation

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ function App() {
         <Route path='/about' element={<About />} />
         <Route path='/contact' element={<Contact />} />
         <Route path='/test' element={<Test />} />
+        <Route path='/useState' element={<UseState />} />
       </Routes>
     </BrowserRouter>
   )
